7f733d056a1f545fa66345488e7f7ae324668f9d
Xylia Rippy <rippyn@oregonstate.edu>
2024-10-28 10:50:13 -0700

    added new classes in the code

--- week_5/In-calss/Inclass51.rb
+++ week_5/In-calss/Inclass51.rb
@@ -35,3 +35,67 @@ class Player
 end
 
 p = Player.new("Alfredo")
+
+
+class Stats
+  attr_accessor :str, :dex, :con, :int, :wis, :cha
+
+  def initialize(str, dex, con, int, wis, cha)
+    @str = str
+    @dex = dex
+    @con = con
+    @int = int
+    @wis = wis
+    @cha = cha
+  end
+end
+
+class Armor
+  attr_accessor :name, :ac
+
+  def initialize(name, ac)
+    @name = name
+    @ac = ac
+  end
+end
+
+class Weapon
+  attr_accessor :name, :damage
+
+  def initialize(name, damage)
+    @name = name
+    @damage = damage
+  end
+end
+
+class Player
+  attr_accessor :name, :stats, :armor, :weapon
+
+  def initialize(name)
+    @name = name
+    @hp = 10
+    @stats = Stats.new(9, 12, 8, 18, 10, 2)  # Initialize stats with given values
+    @armor = [
+      Armor.new("iron helmet", 8),
+      Armor.new("chain mail", 16)
+    ]
+    @weapon = Weapon.new("short sword", "1d6")
+  end
+
+  def damage(points)
+    @hp -= points
+  end
+  def compute_damage(base)
+    # Placeholder for complex damage calculation based on AC and other factors
+  end
+end
+
+p = Player.new("Alfredo")
+puts p.name
+puts p.hp
+puts p.stats.int
+puts p.armor[0].name
+puts p.weapon.damage
+
+
+
